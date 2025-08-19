e8fc81299bf68ecdb4f7ecd8d254bf6883e87bd6
PedroHFP04 <pedrof00412@gmail.com>
2025-08-19 16:22:17 -0300

    Alteracao na DadosSerie

--- src/main/java/b/rcom/exampleAlura/screenmatch/model/DadosSerie.java
+++ src/main/java/b/rcom/exampleAlura/screenmatch/model/DadosSerie.java
@@ -8,8 +8,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true) // para poder ignorar aquilo que agente nao pediu aqui em baixo
 public record DadosSerie(@JsonAlias({"Title", "Titulo"}) String titulo,// sabemos que ele nao consegue transformar title em titulo direto, por isso usamos
                          @JsonAlias("totalSeasons") Integer totalTemporadas,// JsonAlias para poder mostrar esse 'apelido', caso usamos outra API, poderiamos colocar outro nome para ele poder procurar tambem
-                         @JsonAlias("imdbRating") String avaliacao) // // digitar na frente dele o nome certo escrito aonde quer ler
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonAlias({"Year", "Ano"}) String ano,
+                         @JsonAlias("Runtime") String duracao,
+                         @JsonAlias("Genre") String genero)// // digitar na frente dele o nome certo escrito aonde quer ler
 // @JsonProperty("imdbVotes") String votos) { // ele serve tanto para ler o imdbvotes e escever o imdbvotes tambem, diferente do JsonAlias
-// {
-
+{
 }
+
